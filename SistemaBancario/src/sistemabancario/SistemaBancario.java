2ab088c32a2bc5328f733b79b4041357d01f28ce
Edgar Chacon <edgar.aldemar@gmail.com>
2012-12-09 11:51:32 -0530

    Update SistemaBancario/src/sistemabancario/SistemaBancario.java
    Nueva Opción de Parámetros Bancarios (Tipo de Cuentas y Tipos de Operaciones)

--- SistemaBancario/src/sistemabancario/SistemaBancario.java
+++ SistemaBancario/src/sistemabancario/SistemaBancario.java
@@ -19,7 +19,7 @@ public class SistemaBancario {
       BufferedReader     leer     = new BufferedReader(lector);
       int opcion=-1;
       String Respuesta = "S";       
-        while(opcion < 5 && (Respuesta.equals("S")||Respuesta.equals("s"))){
+        while(opcion < 6 && (Respuesta.equals("S")||Respuesta.equals("s"))){
             try {
                 opcion = MenuPrincipal();
             } catch (IOException ex) {
@@ -34,12 +34,14 @@ public class SistemaBancario {
                         break;
                       case 3: 
                         break;
-                      case 4: System.exit(0);
+                      case 4: 
                           break;
+                      case 5: 
+                          Parametros();
                       default: System.exit(0);
             }
             
-            if(opcion<5)
+            if(opcion<6)
             {
                 System.out.println("\nDesea Volver al Menú Principal (S/N): ");
                 try {
@@ -57,8 +59,9 @@ public class SistemaBancario {
      System.out.println("--------Sistema Bancario-------");
      System.out.println("1.- Cuentas Bancarias");
      System.out.println("2.- Operaciones Bancarias");
-     System.out.println("3.- Reposrtes del Sistema");
-     System.out.println("4.- Salir");
+     System.out.println("3.- Reportes del Sistema");
+     System.out.println("4.- Parámetros Bancarios");
+     System.out.println("5.- Salir");
      System.out.println("Seleccione una Opcion:  ");
      return  ( Integer.parseInt(leer.readLine()) );
      }
@@ -111,4 +114,52 @@ public class SistemaBancario {
         System.out.println("Seleccione su opcion: ");
         return  ( Integer.parseInt(leer.readLine()) );
     }
+
+     private static void Parametros() {
+         {
+      InputStreamReader  lector   = new InputStreamReader(System.in);
+      BufferedReader     leer     = new BufferedReader(lector);
+      int opcion=-1;
+      String Respuesta = "S";       
+        while(opcion < 4 && (Respuesta.equals("S")||Respuesta.equals("s"))){
+            try {
+                opcion = MenuParametros();
+            } catch (IOException ex) {
+                Logger.getLogger(SistemaBancario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            switch (opcion)
+            {
+                      case 1: 
+                          break;                    
+                      case 2:
+                        break;
+                      case 3: 
+                          break;
+                      default: System.exit(0);
+            }
+            
+            if(opcion<4)
+            {
+                System.out.println("\nDesea Volver al Menú Principal (S/N): ");
+                try {
+                    Respuesta = leer.readLine();
+                } catch (IOException ex) {
+                    Logger.getLogger(SistemaBancario.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+      }
+    }
+    }
+    
+     private static int MenuParametros() throws IOException {
+     InputStreamReader lector = new InputStreamReader(System.in);
+     BufferedReader      leer = new BufferedReader(lector);
+        System.out.println("-------Cuentas-------------");
+        System.out.println("1.- Tipo de Cuentas");
+        System.out.println("2.- Tipo de Operaciones");
+        System.out.println("3.- Salir.");
+        System.out.println("Seleccione su opcion: ");
+        return  ( Integer.parseInt(leer.readLine()) );
+    }
+
  }
